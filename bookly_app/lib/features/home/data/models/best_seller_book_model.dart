77727ab2fffbf1198c1c6de002291ff9ea743417
Rehab Hamdy <209379697+rehab-hamdi-selim@users.noreply.github.com>
2025-05-02 20:13:03 +0300

    best seller book model

---
+++ bookly_app/lib/features/home/data/models/best_seller_book_model.dart
@@ -0,0 +1,17 @@
+class BestSellerBookModel {
+  final String image;
+  final String bookName;
+  final String bookAuthorName;
+  final String price;
+  final String rate;
+  final String numberOfUSerRate;
+
+  BestSellerBookModel({
+    required this.image,
+    required this.bookName,
+    required this.bookAuthorName,
+    required this.price,
+    required this.rate,
+    required this.numberOfUSerRate,
+  });
+}
