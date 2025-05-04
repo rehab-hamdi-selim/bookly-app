bb3677374e8dce063146a86ea0d6684a5d105f86
Rehab Hamdy <209379697+rehab-hamdi-selim@users.noreply.github.com>
2025-05-04 18:06:48 +0300

    api services

--- bookly_app/lib/core/networking/api_services.dart
+++ bookly_app/lib/core/networking/api_services.dart
@@ -1,12 +1,12 @@
 import 'package:dio/dio.dart';
 
 class ApiServices {
-  final Dio dio;
-  ApiServices(this.dio);
-  final String baseUrl = "https://www.googleapis.com/books/";
+  final Dio _dio;
+  ApiServices(this._dio);
+  final String _baseUrl = "https://www.googleapis.com/books/";
 
   Future<dynamic> getData({required String endpoint}) async {
-    var response = await dio.get('$baseUrl$endpoint');
+    var response = await _dio.get('$_baseUrl$endpoint');
     return response.data;
   }
 }
